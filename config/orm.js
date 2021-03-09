cb1a61c249070ab8ae02b8ab0a9203308616a18a
Maggie may <maggiemaywilder@gmail.com>
2021-03-09 09:32:43 -0600

    set up ORM

---
+++ config/orm.js
@@ -0,0 +1,18 @@
+const connection = require('./connection');
+
+const ORM = {
+    selectAll() {
+
+    },
+
+    insertOne() {
+
+    },
+
+    updateOne() {
+
+    },
+
+};
+
+module.exports = ORM;
