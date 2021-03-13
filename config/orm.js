337f4e9b3f23bf1c429a75cc27118fa34c116269
Maggie may <maggiemaywilder@gmail.com>
2021-03-13 13:53:26 -0600

    create ORM methods

--- config/orm.js
+++ config/orm.js
@@ -2,108 +2,44 @@
 const connection = require('./connection');
 
 const ORM = {
-    selectAll() {
-
+    selectAll(tableInput, cb) {
+        const queryString = `SELECT * FROM ${tableInput}`;
+        connection.query(queryString, (err, result) => {
+            if (err) throw err;                ;
+            cb(result);
+        });
     },
 
-    insertOne() {
+    // define all values in params, no hard coding
+    insertOne(table, cols, val, cb) {
+        let queryString = `INSERT INTO ${table}`;
+        queryString += ` (${cols}) `
+        `VALUES (${val})`;
+        console.log(queryString);
 
+        connection.query(queryString, (err, res) => {
+            if (err) throw err;
+            console.log(res)
+            cb(res);
+        }); 
     },
 
-    updateOne() {
+    // I don't understand the objColVals and the second queryString += value
+    updateOne(table, objColVals, condition, cb) {
+        let queryString = `UPDATE ${table}`
+        queryString += ` SET `;
+        queryString += objToSql(objColVals);
+        queryString += ` WHERE `;
+        queryString += condition;
 
+        console.log(queryString);
+        connection.query(queryString, (err, res) => {
+            if (err) throw err;
+            console.log(res);
+            cb(res);
+        });
     },
 
 };
-
-// Code from 13-17-CatsApp in class exercise- same layout, taking notes on where stuff goes and how
-
-// // Helper function to convert object key/value pairs to SQL syntax
-// const objToSql = (ob) => {
-//   const arr = [];
-//   // Loop through the keys and push the key/value as a string int arr
-//   for (const key in ob) {
-//     let value = ob[key];
-//     // Check to skip hidden properties
-//     if (Object.hasOwnProperty.call(ob, key)) {
-//       // If string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-//       if (typeof value === 'string' && value.indexOf(' ') >= 0) {
-//         value = `'${value}'`;
-//       }
-//       // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-//       // e.g. {sleepy: true} => ["sleepy=true"]
-//       arr.push(`${key}=${value}`);
-//     }
-//   }
-//   // Translate array of strings to a single comma-separated string
-//   return arr.toString();
-// };
-
-// // Object for all our SQL statement functions.
-// const orm = {
-//   all(tableInput, cb) {
-//     const queryString = `SELECT * FROM ${tableInput};`;
-//     connection.query(queryString, (err, result) => {
-//       if (err) {
-//         throw err;
-//       }
-//       cb(result);
-//     });
-//   },
-//   create(table, cols, vals, cb) {
-//     let queryString = `INSERT INTO ${table}`;
-
-//     queryString += ' (';
-//     queryString += cols.toString();
-//     queryString += ') ';
-//     queryString += 'VALUES (';
-//     queryString += printQuestionMarks(vals.length);
-//     queryString += ') ';
-
-//     console.log(queryString);
-
-//     connection.query(queryString, vals, (err, result) => {
-//       if (err) {
-//         throw err;
-//       }
-
-//       cb(result);
-//     });
-//   },
-//   // An example of objColVals would be {name: panther, sleepy: true}
-//   update(table, objColVals, condition, cb) {
-//     let queryString = `UPDATE ${table}`;
-
-//     queryString += ' SET ';
-//     queryString += objToSql(objColVals);
-//     queryString += ' WHERE ';
-//     queryString += condition;
-
-//     console.log(queryString);
-//     connection.query(queryString, (err, result) => {
-//       if (err) {
-//         throw err;
-//       }
-
-//       cb(result);
-//     });
-//   },
-//   delete(table, condition, cb) {
-//     let queryString = `DELETE FROM ${table}`;
-//     queryString += ' WHERE ';
-//     queryString += condition;
-
-//     connection.query(queryString, (err, result) => {
-//       if (err) {
-//         throw err;
-//       }
-
-//       cb(result);
-//     });
-//   },
-// };
-
-// // Export the orm object for the model (cat.js).
-// module.exports = orm;
 
 module.exports = ORM;
