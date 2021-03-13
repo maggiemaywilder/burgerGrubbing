e7ba9c97fc523380e9a0616f9eac6741f6cda310
Maggie may <maggiemaywilder@gmail.com>
2021-03-13 14:26:20 -0600

    fix table structure to contain devoured

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,7 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INTEGER AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(100) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
 
