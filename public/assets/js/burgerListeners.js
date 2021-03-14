30eff45bf3635daeb6be944d52c45189f4a2307e
Maggie may <maggiemaywilder@gmail.com>
2021-03-13 21:37:21 -0600

    adding event listeners

--- public/assets/js/burgerListeners.js
+++ public/assets/js/burgerListeners.js
@@ -1,50 +1,78 @@
 // // Event listeners and buttons
-// code from catsApp for inspiration
+// no listeners until DOM loaded
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (event) {
+        console.info('DOM loaded');
+    }
 
-// // Make sure we wait to attach our handlers until the DOM is fully loaded.
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     if (event) {
-//       console.info('DOM loaded');
-//     }
-  
-//     // UPDATE
-//     const changeSleepBtns = document.querySelectorAll('.change-sleep');
-  
-//     // Set up the event listener for the create button
-//     if (changeSleepBtns) {
-//       changeSleepBtns.forEach((button) => {
-//         button.addEventListener('click', (e) => {
-//           // Grabs the id of the element that goes by the name, "id"
-//           const id = e.target.getAttribute('data-id');
-//           const newSleep = e.target.getAttribute('data-newsleep');
-  
-//           const newSleepState = {
-//             sleepy: newSleep,
-//           };
-  
-//           fetch(`/api/cats/${id}`, {
-//             method: 'PUT',
-//             headers: {
-//               Accept: 'application/json',
-//               'Content-Type': 'application/json',
-//             },
-  
-//             // make sure to serialize the JSON body
-//             body: JSON.stringify(newSleepState),
-//           }).then((response) => {
-//             // Check that the response is all good
-//             // Reload the page so the user can see the new quote
-//             if (response.ok) {
-//               console.log(`changed sleep to: ${newSleep}`);
-//               location.reload('/');
-//             } else {
-//               alert('something went wrong!');
-//             }
-//           });
-//         });
-//       });
-//     }
-  
+    // need devour buttons
+    const devourBtns = document.querySelectorAll('.devourBtn');
+    // need buttons w/ class devourBtn to populate with burgers devoured=false in handlebars file
+
+    // set up listeners for buttons created in handlebars by #each
+    if (devourBtns) {
+        devourBtns.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                // need id (primary key) from table?
+                const id = e.target.getAttribute('data-id');
+                // do I need a variable to target the devoured state?
+
+                const devourIt = {
+                    devoured: true,
+                }
+
+                fetch(`/api/cats/${id}`, {
+                    method: 'PUT',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(devourIt),
+                }) .then((response) => {
+                    // if response is ok, reload page with burger switched to devoured side
+                    if (response.ok) {
+                        console.log(`Burger ${id} has been devoured.`);
+                        location.reload('/');
+                    } else {
+                        alert('Something went wrong!')
+                    }
+                });
+            });
+        });
+    };
+
+    // button to add burger to list
+    const makeBurgerBtn = document.getElementById('makeBurger');
+
+    if(makeBurgerBtn) {
+        makeBurgerBtn.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const newBurger = {
+                burger_name: document.getElementById('bg').value.trim(),
+                // don't need devoured because default = false
+            };
+        
+        // need POST request to create new burger
+            fetch('api/burgers', {
+                method: 'POST',
+                header: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newBurger),
+            }).then(() => {
+                // empty form
+                document.getElementById('bg').value = '';
+                console.log('Order up!');
+                location.reload();
+            });
+        });
+    }
+    
+});
+
+
 //     // CREATE
 //     const createCatBtn = document.getElementById('create-form');
   
