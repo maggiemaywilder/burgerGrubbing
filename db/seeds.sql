12674d5f536a8beb94b3ecbb584baebc64642e85
Maggie may <maggiemaywilder@gmail.com>
2021-03-13 20:52:16 -0600

    update to seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,6 @@
 USE burgers_db;
 
 -- insert queries to populate burgers table with at least 3 entries --
+-- not putting the devoured column should populate to false throughout, correct? --
 INSERT INTO burgers (burger_name)
 VALUES ('Mushroom Swiss Burger'), ('Turkey Burger'), ('Double Bacon Cheeseburger');
