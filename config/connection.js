b4e3d3ba71629dd04b8104d8b2c61f19af2e225b
Maggie may <maggiemaywilder@gmail.com>
2021-03-16 23:26:19 -0500

    deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 const mysql = require('mysql');
 const password = require('../private/connect');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: `${password}`,
-    database: 'burgers_db',
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: `${password}`,
+        database: 'burgers_db',
+    });
+}
+
 
 connection.connect((err) => {
     if (err) {
